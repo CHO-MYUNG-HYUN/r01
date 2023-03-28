c5ebd4a09c925cb947f283a41b0fac7aa89140c1
CHO-MYUNG-HYUN <whaudgus123@naver.com>
2023-03-28 18:20:15 +0900

    summary

---
+++ javaProject/MyProject/src/contact2/ContactMain.java
@@ -0,0 +1,19 @@
+package contact2;
+
+public class ContactMain {
+
+	public static void main(String[] args) {
+
+		Contact c = new Contact(
+				"asd", 
+				"null", 
+				"sdfnull", 
+				"ll", 
+				"sfd", 
+				"ull12");
+		
+		System.out.println("이름 : "+ c.getName());
+
+	}
+
+}
