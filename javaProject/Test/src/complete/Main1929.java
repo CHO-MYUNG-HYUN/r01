a9158464e612b15a8b2b4435888c95186eccd7cc
CHO-MYUNG-HYUN <whaudgus123@naver.com>
2023-04-07 18:19:29 +0900

    summary

--- javaProject/Test/src/complete/Main1929.java
+++ javaProject/Test/src/complete/Main1929.java
@@ -3,7 +3,8 @@ package complete;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main1929 {
+public class Main1929 { 
+	//04월 07일 , 에라토스테네스의 체, 소수 찾기 
 
 	public static void main(String[] args) {
 
