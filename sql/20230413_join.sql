2f78a8f26e00311699cac07f1b3bdd317ee20656
CHO-MYUNG-HYUN <whaudgus123@naver.com>
2023-04-13 18:16:27 +0900

    summary

--- sql/20230413_join.sql
+++ sql/20230413_join.sql
@@ -1,6 +1,5 @@
 --2023.04.13
 
---
 select *
 from emp, dept 
 where emp.deptno = dept.deptno
@@ -21,3 +20,49 @@ from emp e, dept d
 where e.deptno = d.deptno
 and e.ename='SCOTT'
 ;
+
+--사원별 급여 등급 표현
+select * from salgrade;
+
+select * 
+from emp e, salgrade s
+where e.sal between s.losal and s.hisal;
+
+
+-- self join : 자기 자신의 테이블을 조인하는 경우
+SELECT e.ename, nvl(m.ename, '없음')
+from emp e, emp m
+WHERE e.mgr = m.empno;
+-- 각 사원의 이름과 각 사원의 매니저 이름을 출력
+--outer join : 조건에 만족하지 않는 행도 출력을 하는 join
+SELECT e.ename, nvl(m.ename, '없음')
+from emp e, emp m
+WHERE e.mgr = m.empno(+);
+
+--cross join
+select *
+from emp, dept;
+
+select *
+from emp CROSS join dept;
+
+select *
+from emp inner join dept
+on emp.deptno = dept.deptno;
+
+select *
+from emp join dept
+on emp.deptno = dept.deptno;
+
+select *
+from emp NATURAL join dept; --조인 조건을 생략할 수 있다.
+
+select *
+from emp join dept
+using(deptno); --같은 이름의 컬럼이 필요하다.
+
+select *
+from emp e right outer join emp m
+on e.mgr = m.empno;
+
+-----------------------------------
---
+++ sql/join&subQuery_exam.sql
@@ -0,0 +1,148 @@
+--1 마당서점의고객이요구하는다음질문에대해SQL 문을작성하시오.
+
+--(5) 박지성이구매한도서의출판사수
+SELECT
+    COUNT(publisher)
+FROM
+         book b
+    JOIN (
+        SELECT
+            bookid
+        FROM
+                 orders o
+            JOIN customer c USING ( custid )
+        WHERE
+            name = '박지성'
+    ) p USING ( bookid );
+
+SELECT
+    COUNT(publisher)
+FROM
+    book b
+WHERE
+    bookid IN (
+        SELECT
+            bookid
+        FROM
+                 orders o
+            JOIN customer c USING ( custid )
+        WHERE
+            name = '박지성'
+    );
+    
+    
+    
+    
+    
+--(6) 박지성이구매한도서의이름, 가격, 정가와판매가격의차이
+SELECT
+    bookname,
+    b.price - p.saleprice AS pricegap
+FROM
+         book b
+    JOIN (
+        SELECT
+            o.bookid,
+            o.saleprice
+        FROM
+                 orders o
+            JOIN customer c USING ( custid )
+        WHERE
+            name = '박지성'
+    ) p USING ( bookid );
+    
+
+    
+--(7) 박지성이구매하지않은도서의이름
+--from 절
+SELECT
+    b.bookname
+FROM
+         book b
+    JOIN (
+        SELECT
+            o.bookid
+        FROM
+                 orders o
+            JOIN customer c USING ( custid )
+        WHERE
+            name != '박지성'
+    ) p ON b.bookid = p.bookid;
+    
+--where절
+SELECT
+    bookname
+FROM
+    book
+WHERE
+    bookid NOT IN (
+        SELECT
+            o.bookid
+        FROM
+                 orders o
+            JOIN customer c USING ( custid )
+        WHERE
+            name = '박지성'
+    );
+
+--2 마당서점의운영자와경영자가요구하는다음질문에대해SQL 문을작성하시오.
+--(8) 주문하지않은고객의이름(부속질의사용)
+SELECT
+    c.name
+FROM
+    customer c
+WHERE
+    c.custid NOT IN (
+        SELECT
+            custid
+        FROM
+            orders
+    );
+
+
+--(9) 주문금액의총액과주문의평균금액
+SELECT
+    SUM(saleprice) AS total,
+    AVG(saleprice) AS avgsaleprice
+FROM
+    orders;
+    
+
+--(10) 고객의이름과고객별구매액 
+SELECT
+    c.name,
+    SUM(o.saleprice)
+FROM
+         customer c
+    JOIN orders o USING ( custid )
+GROUP BY
+    name;
+    
+    
+--(11) 고객의이름과고객이구매한도서목록
+SELECT
+    c.name,
+    b.bookname
+FROM
+         customer c
+    JOIN orders o ON c.custid = o.custid
+    JOIN book   b ON b.bookid = o.bookid;
+
+
+--(12) 도서의가격(Book 테이블)과판매가격(Orders 테이블)의차이가가장많은주문
+SELECT
+    *
+FROM
+         orders o
+    JOIN book b USING ( bookid )
+WHERE
+    abs(b.price - o.saleprice) = (
+        SELECT
+            MAX(abs(b.price - o.saleprice))
+        FROM
+                 book b
+            JOIN orders o USING ( bookid )
+    );
+    
+    
+--(13) 도서의판매액평균보다자신의구매액평균이더높은고객의이름
