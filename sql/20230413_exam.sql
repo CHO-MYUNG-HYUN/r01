8346065dbdd5c0bd492a5049fe526e71a74b60d8
CHO-MYUNG-HYUN <whaudgus123@naver.com>
2023-04-13 16:08:39 +0900

    Update 20230413_exam.sql

--- sql/20230413_exam.sql
+++ sql/20230413_exam.sql
@@ -1,25 +1,45 @@
 --32. EQUI 조인을 사용하여 SCOTT 사원의 부서번호와 부서 이름을 출력하시오.
-select d.deptno, d.dname
-from emp e, dept d
-where e.deptno = d.deptno
-and e.ename = 'SCOTT';
+SELECT
+    d.deptno,
+    d.dname
+FROM
+    emp  e,
+    dept d
+WHERE
+        e.deptno = d.deptno
+    AND e.ename = 'SCOTT';
 
 --33. INNER JOIN과 ON 연산자를 사용하여 사원 이름과 함께 그 사원이 소속된 부서이름과 지역 명을 출력하시오.
-select e.ename, d.dname, d.loc
-from emp e inner join dept d
-on e.deptno = d.deptno;
+SELECT
+    e.ename,
+    d.dname,
+    d.loc
+FROM
+         emp e
+    INNER JOIN dept d ON e.deptno = d.deptno;
 
 --36. 조인과 WildCARD를 사용하여 이름에 ‘A’가 포함된 모든 사원의 이름과 부서명을 출력하시오.
-select e.ename, d.dname
-from emp e join dept d
-using(deptno)
-where e.ename like '%A%';
+SELECT
+    e.ename,
+    d.dname
+FROM
+         emp e
+    JOIN dept d USING ( deptno )
+WHERE
+    e.ename LIKE '%A%';
 
 --37. JOIN을 이용하여 NEW YORK에 근무하는 모든 사원의 이름, 업무, 부서번호 및 부서명을 출력하시오.
-select e.ename, e.job, deptno, d.dname, d.loc
-from emp e join dept d
-using(deptno)
-where d.loc ='NEW YORK';
+SELECT
+    e.ename,
+    e.job,
+    deptno,
+    d.dname,
+    d.loc
+FROM
+         emp e
+    JOIN dept d USING ( deptno )
+WHERE
+    d.loc = 'NEW YORK';
 
 --select e.ename, e.job, e.deptno, d.dname, d.loc
 --from emp e join dept d
@@ -27,19 +47,33 @@ where d.loc ='NEW YORK';
 --where d.loc ='NEW YORK';
 
 --38. SELF JOIN을 사용하여 사원의 이름 및 사원번호, 관리자 이름을 출력하시오.
-select e.ename, e.empno, m.mgr
-from emp e join emp m
-on e.mgr = m.empno;
+SELECT
+    e.ename,
+    e.empno,
+    m.mgr
+FROM
+         emp e
+    JOIN emp m ON e.mgr = m.empno;
 
 --39. OUTER JOIN, SELF JOIN을 사용하여 관리자가 없는 사원을 포함하여 사원번호를 기준으로 내림차순 정렬하여 출력하시오.
-select e.ename, e.empno, m.mgr
-from emp e left outer join emp m
-on e.mgr = m.empno
-order by e.empno desc;
+SELECT
+    e.ename,
+    e.empno,
+    m.mgr
+FROM
+    emp e
+    LEFT OUTER JOIN emp m ON e.mgr = m.empno
+ORDER BY
+    e.empno DESC;
 
 --42. SELF JOIN 을 사용하여 관리자보다 먼저 입사한 모든 사원의 이름 및 입사일을 관리자의 이름 및 입사일과 함께 출력하시오.
-select e.ename as "사원 이름", e.hiredate as "사원 입사일", m.ename as "관리자 이름", m.hiredate as "관리자 입사일"
-from emp e join emp m
-on e.mgr = m.empno
-where e.hiredate < m.hiredate;
-
+SELECT
+    e.ename    AS "사원 이름",
+    e.hiredate AS "사원 입사일",
+    m.ename    AS "관리자 이름",
+    m.hiredate AS "관리자 입사일"
+FROM
+         emp e
+    JOIN emp m ON e.mgr = m.empno
+WHERE
+    e.hiredate < m.hiredate;
