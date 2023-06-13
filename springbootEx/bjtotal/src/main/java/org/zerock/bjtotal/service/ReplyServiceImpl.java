ff64a7d605238493af9954d481a580d728a8c7e1
CHO-MYUNG-HYUN <whaudgus123@naver.com>
2023-06-13 18:04:06 +0900

    대댓글 처리

---
+++ springbootEx/bjtotal/src/main/java/org/zerock/bjtotal/service/ReplyServiceImpl.java
@@ -0,0 +1,70 @@
+package org.zerock.bjtotal.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.zerock.bjtotal.dto.PageRequestDTO;
+import org.zerock.bjtotal.dto.PageResponseDTO;
+import org.zerock.bjtotal.dto.ReplyDTO;
+import org.zerock.bjtotal.mappers.ReplyMapper;
+import org.zerock.bjtotal.mappers.TodoMapper;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Service
+@Log4j2
+@RequiredArgsConstructor
+public class ReplyServiceImpl implements ReplyService{
+
+    private final TodoMapper todoMapper;
+    private final ReplyMapper replyMapper;
+
+    @Override
+    public Long register(ReplyDTO replyDTO) {
+        Long result = null;
+        Long gno = replyDTO.getGno();
+            
+        if(gno == 0L){
+        int count = replyMapper.insert(replyDTO);
+
+        if(count != 1){
+            throw new RuntimeException("Reply Insert Exception");
+        }
+
+        Long rno = replyDTO.getRno();
+        replyMapper.updateGno(rno);
+        result = rno;
+
+        }else {
+            int count = replyMapper.insertChild(replyDTO);
+
+            if(count != 1){
+                throw new RuntimeException("Reply Insert Exception");
+            }
+        result = replyDTO.getRno();
+        }
+        return result;
+    }
+
+    @Override
+    public PageResponseDTO<ReplyDTO> getList(Long tno, PageRequestDTO pageRequestDTO) {
+    
+        pageRequestDTO.setSize(100);
+
+        List<ReplyDTO> list = replyMapper.selectList(tno, pageRequestDTO);
+        int total = replyMapper.getTnoCount(tno);
+
+        return PageResponseDTO.<ReplyDTO>withAll()
+        .list(list)
+        .total(total)
+        .build();
+    }
+
+    @Override
+    public ReplyDTO getOne(Long tno) {
+        return replyMapper.selectOne(tno);    
+    }
+    
+    
+}
