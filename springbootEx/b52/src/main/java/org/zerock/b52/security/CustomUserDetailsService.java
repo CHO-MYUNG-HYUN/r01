45d8cede37817d3659aaaa01231a5928a86cacdf
CHO-MYUNG-HYUN <whaudgus123@naver.com>
2023-06-26 11:25:55 +0900

    0626 Security

--- springbootEx/b52/src/main/java/org/zerock/b52/security/CustomUserDetailsService.java
+++ springbootEx/b52/src/main/java/org/zerock/b52/security/CustomUserDetailsService.java
@@ -1,5 +1,8 @@
 package org.zerock.b52.security;
 
+import java.util.List;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -7,6 +10,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.zerock.b52.dto.MemberDTO;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -23,13 +27,23 @@ public class CustomUserDetailsService implements UserDetailsService{
     log.info("loaduser..." + username);
     log.info("=================================");
 
-    UserDetails user = User.builder()
-    .username(username)
-    .password(passwordEncoder.encode("1111"))
-    .authorities("ROLE_USER","ROLE_G1")
-    .build();
+    
+    MemberDTO memberDTO =
+        new MemberDTO(
+            username, 
+      "$2a$10$jLR7mXrXrLknLDelysIHYubSxK7AkrRLbD3SBmZu.mpkQ6hQIge4m", 
+      List.of(
+        new SimpleGrantedAuthority("ROLE_USER")
+        )
+      );
 
-    return user;
+    // UserDetails user = User.builder()
+    // .username(username)
+    // .password(passwordEncoder.encode("1111"))
+    // .authorities("ROLE_USER","ROLE_G1")
+    // .build();
+
+    return memberDTO;
   }
   
 }
