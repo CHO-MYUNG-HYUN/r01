cead51884ea4aecdec88a9033868bbbcd75bb423
CHO-MYUNG-HYUN <whaudgus123@naver.com>
2023-06-26 13:57:29 +0900

    0626 Security

--- springbootEx/b52/src/main/java/org/zerock/b52/security/CustomUserDetailsService.java
+++ springbootEx/b52/src/main/java/org/zerock/b52/security/CustomUserDetailsService.java
@@ -11,13 +11,18 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.zerock.b52.dto.MemberDTO;
+import org.zerock.b52.dto.MemberReadDTO;
+import org.zerock.b52.mappers.MemberMapper;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 @Service
+@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService{
 
+  private final MemberMapper memberMapper;
   private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
   @Override
@@ -27,15 +32,14 @@ public class CustomUserDetailsService implements UserDetailsService{
     log.info("loaduser..." + username);
     log.info("=================================");
 
+    MemberReadDTO readDTO = memberMapper.selectOne(username);
     
-    MemberDTO memberDTO =
-        new MemberDTO(
-            username, 
-      "$2a$10$jLR7mXrXrLknLDelysIHYubSxK7AkrRLbD3SBmZu.mpkQ6hQIge4m", 
-      List.of(
-        new SimpleGrantedAuthority("ROLE_USER")
-        )
-      );
+    MemberDTO memberDTO = new MemberDTO(
+      username,
+      readDTO.getMpw(),
+      readDTO.getMname(),
+      readDTO.getRolenames()
+    );
 
     // UserDetails user = User.builder()
     // .username(username)
