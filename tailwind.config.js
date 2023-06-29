f5fcfe133017c11fb0db0374972be0a9db434811
CHO-MYUNG-HYUN <whaudgus123@naver.com>
2023-06-29 09:36:07 +0900

    0628 React Kiosk

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'm1c': '#F5F5F5',
+        'm2c': '#F2EAD3',
+        'm3c': '#DFD7BF',
+        'm4c': '#3F2305'
+      }
+    },
   },
   plugins: [],
 }
